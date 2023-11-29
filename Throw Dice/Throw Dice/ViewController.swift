3ff3ce83b00b63913449cfb004301b3911033f00
Halit Bakır <halitbakirx@gmail.com>
2023-11-29 17:01:04 +0300

    Update ViewController.swift

--- Throw Dice/Throw Dice/ViewController.swift
+++ Throw Dice/Throw Dice/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var left: UIImageView!
     @IBOutlet weak var right: UIImageView!
     
-    //zar görselleri tanımlıyoruz
     let gorsel = [UIImage(named: "1"), UIImage(named: "2"), UIImage(named: "3"), UIImage(named: "4"), UIImage(named: "5"), UIImage(named: "6")]
     
     override func viewDidLoad() {
@@ -22,12 +21,10 @@ class ViewController: UIViewController {
 
     @IBAction func zarAt(_ sender: Any) {
         
-        //her zar için 6ya kadar random bir sayı oluşturmasını istiyoruz
         let first = Int(arc4random_uniform(6))
         let second = Int(arc4random_uniform(6))
         
         
-        //gelen ilk görseli sola 2.yi sağa eklicez
         left.image = gorsel[first]
         right.image = gorsel[second]
         
